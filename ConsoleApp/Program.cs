80b06eff5969c319a7fa154928f24cce8c1c40f5
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-04-06 16:24:18 +0800

    转义

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp
         static void Main()
         {
             Console.WriteLine($"void Console.WriteLine(string? value): {content}");
-            Console.MarkupLine($"void SystemConsoleExtensions.extension(Console).MarkupLine(string markup) [DuckStudio.CatFood 2.0.0.2]: {content}");
+            Console.MarkupLine($"void SystemConsoleExtensions.extension(Console).MarkupLine(string markup) [[DuckStudio.CatFood 2.0.0.2]]: {content}");
             Print.PrintWithPrefix(content, Print.MSHead.Success, markuped: true);
 
-            AnsiConsole.MarkupLine($"void AnsiConsole.MarkupLine(string value) [DuckStudio.CatFood 2.0.0.1]: {content}");
+            AnsiConsole.MarkupLine($"void AnsiConsole.MarkupLine(string value) [[DuckStudio.CatFood 2.0.0.1]]: {content}");
         }
     }
 }
