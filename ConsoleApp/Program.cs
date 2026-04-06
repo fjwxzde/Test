c86da03d00ab5f9016aba0bab55c3f671763389d
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-04-06 15:08:50 +0800

    添加一个测试项目

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DuckStudio.CatFood.Functions;
+using Spectre.Console;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private const string content = "[yellow]Hello, World![/]";
+
+        static void Main()
+        {
+            Console.WriteLine(content);
+            Console.MarkupLine(content);
+            Print.PrintWithPrefix(content, Print.MSHead.Success, markuped: true);
+
+            AnsiConsole.MarkupLine(content);
+        }
+    }
+}
