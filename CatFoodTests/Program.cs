f6672f370a2f651d344a81396507a8876125b3be
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-04-08 21:23:35 +0800

    添加一些简单的 DuckStudio.CatFood 的测试 (#52)
    * 添加一些简单的 DuckStudio.CatFood 的测试
    
    * 固定工作流版本到全长哈希
    
    * 补全 GITHUB_TOKEN 权限限制
    
    * 添加 persist-credentials: false
    
    by zizmor .github/workflows/ --fix=all

---
+++ CatFoodTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using DuckStudio.CatFood.Functions;
+
+namespace CatFoodTests
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("测试 Print.PrintWithPrefix");
+            Print.PrintWithPrefix("[yellow]预期这条内容是黄色的[/]", Print.MSHead.Question, markuped: true);
+            Print.PrintWithPrefix("[yellow][预期这条内容没有颜色][/]", Print.MSHead.Question);
+
+            Console.WriteLine();
+            Console.WriteLine("测试 Print.PrintMultilineWithPrefix");
+            Print.PrintMultilineWithPrefix(["这是", "多行", "[blue]内容[/]", "输出", "(禁用标记)"], "-");
+            Console.WriteLine();
+            Print.PrintMultilineWithPrefix(["这是", "多行", "[blue]内容[/]", "输出", "(启用标记)"], "-", markuped: true);
+        }
+    }
+}
---
+++ DuckStudio.CatFood.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿using DuckStudio.CatFood.Functions;
+
+namespace DuckStudio.CatFood.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestTerminalCalculateCharactersDisplayed()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                Assert.AreEqual(9, Terminal.CalculateCharactersDisplayed("六个字33a"));
+            }
+        }
+    }
+}
